94b50b5241e6d7ffa900896cebae3cecf2674e05
Samy RABIH <samy.rabih@hubicus.com>
2021-03-20 18:14:22 +0100

    First commit with variables declaration

---
+++ customizable-color-lamp.ino
@@ -0,0 +1,27 @@
+//max value of the signal when the potentiometer is at max
+const int MAX_VALUE = 640;
+
+//Index for color settings
+const int RED = 0;
+const int BLUE = 1;
+const int GREEN = 2;
+int pinsByColor[] = { 
+  10,//pin for RED
+  11,//ping for BLUE
+  12 //pin for GREEN
+};
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+int getColorValueFromPot(int colorIndex, int potIndex) {
+
+}
